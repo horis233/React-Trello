1a9640828f7f44a38b2cca51f9086669a830b2b6
horis233 <horis233@gmail.com>
2018-09-28 23:59:40 -0400

    add List component

--- src/app/components/Board.jsx
+++ src/app/components/Board.jsx
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
-import { Helmet } from "react-helmet";
- class Board extends Component {
-  render = () => (
-    <div className="board">
-      <Helmet>
-        <title>Board name | Trello</title>
-      </Helmet>
-      <h1>Hello its me your board</h1>
-    </div>
-  );
-}
- export default Board;
+import { Helmet } from "react-helmet";	import { Helmet } from "react-helmet";
+import List from "./List";
+ class Board extends Component {	class Board extends Component {
+  constructor() {
+    super();
+    this.state = {
+      lists: [
+        {
+          title: "TODO Big picture",
+          cards: [
+            {
+              title: "Inspect how trello deals with loading boards, images"
+            },
+            { title: "Make skeleton structure of whatever" }
+          ]
+        }
+      ]
+    };
+  }
+  render = () => (	  render = () => (
+    <div className="board">	    <div className="board">
+      <Helmet>	      <Helmet>
+        <title>Board name | Trello</title>	        <title>Board name | Trello</title>
+      </Helmet>	      </Helmet>
+      <h1>Hello its me your board</h1>	      {this.state.lists.map(list => <List list={list} />)}
+    </div>	    </div>
+  );	  );
+}	}
---
+++ src/app/components/List.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+ class List extends Component {
+  render = () => {
+    const { list } = this.props;
+    return (
+      <div className="list">
+        <span className="list-title">{list.title}</span>
+      </div>
+    );
+  };
+}
+ export default List;
