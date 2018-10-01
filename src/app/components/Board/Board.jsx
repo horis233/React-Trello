b37c973b587b29253bf4450724defb66dbca0ecb
horis233 <horis233@gmail.com>
2018-09-30 22:58:56 -0400

    add board title

--- src/app/components/Board/Board.jsx
+++ src/app/components/Board/Board.jsx
@@ -1,17 +1,20 @@
 // @flow
-import React, { Component } from "react";
-import { Helmet } from "react-helmet";
+import React, {Component} from "react";
+import {Helmet} from "react-helmet";
 import List from "./List";
 import "./Board.scss";
 
 type State = {
-  lists: Array<{
+  lists: Array < {
     title: string,
-    cards: Array<{ title: string }>
-  }>
+    cards: Array < {
+      title: string
+    } >
+  } >
 };
 
-class Board extends Component<{}, State> {
+class Board extends Component < {},
+State > {
   constructor() {
     super();
     this.state = {
@@ -21,33 +24,34 @@ class Board extends Component<{}, State> {
           cards: [
             {
               title: "Inspect how trello deals with loading boards, images"
-            },
-            { title: "Make skeleton structure of whatever" }
+            }, {
+              title: "Make skeleton structure of whatever"
+            }
           ]
-        },
-        {
+        }, {
           title: "TODO details",
           cards: [
             {
               title: "Do some stuff"
-            },
-            {
-              title:
-                "Maybe a really really long one: How should I manage board state, and generally state for components that are the same but have different data?"
+            }, {
+              title: "Maybe a really really long one: How should I manage board state, and generally state for components that are the same but have different data?"
             }
           ]
         }
       ]
     };
   }
-  render = () => (
-    <div className="board">
-      <Helmet>
-        <title>Board name | Trello</title>
-      </Helmet>
-      {this.state.lists.map(list => <List list={list} />)}
+  render = () => (<div className="board">
+    <Helmet>
+      <title>Board name | Trello</title>
+    </Helmet>
+    <div className="board-header">
+      <h1 className="board-title">Board name</h1>
     </div>
-  );
+    <div className="lists">
+      {this.state.lists.map(list => <List list={list}/>)}
+    </div>
+  </div>);
 }
 
 export default Board;
