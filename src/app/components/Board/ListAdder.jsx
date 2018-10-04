4e1db72891bb081f7a03a1a0d7764ebdb919f944
horis233 <horis233@gmail.com>
2018-10-04 15:54:32 -0400

    add add-list function in  redux

--- src/app/components/Board/ListAdder.jsx
+++ src/app/components/Board/ListAdder.jsx
@@ -1,22 +1,59 @@
+// @flow
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Textarea from 'react-textarea-autosize';
+import shortid from 'shortid';
 
 type Props = {
-	lists: Array<{ id: string }>,
-	boardTitle: string,
-	boardId: string,
-	dispatch: ({ type: string }) => void
+	dispatch: ({ type: string }) => void,
+	boardId: string
 };
 
-class ListAdder extends Component<Props> {
+type State = {
+	isListInEdit: boolean,
+	newListTitle: string
+};
+
+class ListAdder extends Component<Props, State> {
 	constructor() {
 		super();
 		this.state = {
-			isListInEdit: false
+			isListInEdit: false,
+			newListTitle: ''
 		};
 	}
+
+	handleBlur = () => {
+		this.setState({ isListInEdit: false });
+	};
+
+	handleChange = (event) => {
+		this.setState({ newListTitle: event.target.value });
+	};
+
+	handleKeyDown = (event) => {
+		if (event.keyCode === 13) {
+			event.preventDefault();
+			this.handleSubmit();
+		}
+	};
+
+	handleSubmit = () => {
+		const { dispatch, boardId } = this.props;
+		const { newListTitle } = this.state;
+		dispatch({
+			type: 'ADD_LIST',
+			payload: {
+				ListTitle: newListTitle,
+				listId: shortid.generate(),
+				boardId
+			}
+        });
+        this.setState({ isListInEdit: false, newListTitle:""});
+	};
+
 	render = () => {
-		const { isListInEdit } = this.state;
+		const { isListInEdit, newListTitle } = this.state;
 		if (!isListInEdit) {
 			return (
 				<button onClick={() => this.setState({ isListInEdit: true })} className="add-list-button">
@@ -24,7 +61,21 @@ class ListAdder extends Component<Props> {
 				</button>
 			);
 		}
-		return <div className="list">A List</div>;
+		return (
+            <div className="list">
+              <div className="list-title-textarea-wrapper">
+                <Textarea
+                  autoFocus
+                  useCacheForDOMMeasurements
+                  value={newListTitle}
+                  onChange={this.handleChange}
+                  onKeyDown={this.handleKeyDown}
+                  className="list-title-textarea"
+                  onBlur={this.handleBlur}
+                />
+              </div>
+            </div>
+          );
 	};
 }
 export default connect()(ListAdder);
--- src/app/reducers/reducers.js
+++ src/app/reducers/reducers.js
@@ -29,6 +29,7 @@ type Action = {
 		listTitle: string,
 		cardId: string,
 		cardTitle: string,
+		boardId: string,
 		sourceIndex: number,
 		destinationIndex: number,
 		sourceId: string,
@@ -107,6 +108,13 @@ const lists = (state: ListState = initialListState, action: Action) => {
 				}
 			};
 		}
+		case 'ADD_LIST': {
+			const { listId, listTitle } = action.payload;
+			return {
+				...state,
+				[listId]: { ...state[listId], title: listTitle, cards: [] }
+			};
+		}
 		case 'EDIT_LIST_TITLE': {
 			const { listId, listTitle } = action.payload;
 			return {
@@ -118,7 +126,6 @@ const lists = (state: ListState = initialListState, action: Action) => {
 			};
 		}
 		case 'REORDER_LIST': {
-
 			const { sourceIndex, destinationIndex, sourceId, destinationId } = action.payload;
 			// Reorder within the same list
 			if (sourceId === destinationId) {
@@ -148,16 +155,26 @@ const lists = (state: ListState = initialListState, action: Action) => {
 
 const boards = (state: BoardState = initialBoardState, action: Action) => {
 	switch (action.type) {
-    case "REORDER_BOARD": {
-      const { sourceIndex, destinationIndex, sourceId } = action.payload;
-      const newLists = Array.from(state[sourceId].lists);
-      const [removedList] = newLists.splice(sourceIndex, 1);
-      newLists.splice(destinationIndex, 0, removedList);
-      return {
-        ...state,
-        [sourceId]: { ...state[sourceId], lists: newLists }
-      };
-    }
+		case 'REORDER_BOARD': {
+			const { sourceIndex, destinationIndex, sourceId } = action.payload;
+			const newLists = Array.from(state[sourceId].lists);
+			const [ removedList ] = newLists.splice(sourceIndex, 1);
+			newLists.splice(destinationIndex, 0, removedList);
+			return {
+				...state,
+				[sourceId]: { ...state[sourceId], lists: newLists }
+			};
+		}
+		case 'ADD_LIST': {
+			const { boardId, listId } = action.payload;
+			return {
+				...state,
+				[boardId]: {
+					...state[boardId],
+					lists: [ ...state[boardId].lists, listId ]
+				}
+			};
+		}
 		default:
 			return state;
 	}
