5eb98c8eb13b23c3131a1c715c1f2b6c57189ae1
horis233 <horis233@gmail.com>
2018-10-07 22:17:58 -0400

    update logo

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,12 +1,17 @@
-import React, {Component} from "react";
-import trelloLogo from "../assets/trello-logo-white.svg";
+import React, { Component } from 'react';
+//import trelloLogo from "../assets/trello-logo-white.svg";
+import trelloLogo from '../../assets/images/trello-logo.png';
 
 class Header extends Component {
-  render = () => (<header style={{
-      background: "#026aa7",
-      padding: 5
-    }}>
-    <img src={trelloLogo} alt="Trello logo"/>
-  </header>);
+	render = () => (
+		<header
+			style={{
+				background: '#026aa7',
+				padding: 5
+			}}
+		>
+			<img src={trelloLogo} alt="Trello logo" />
+		</header>
+	);
 }
 export default Header;
--- src/app/components/Home.jsx
+++ src/app/components/Home.jsx
@@ -1,35 +1,36 @@
-import React, {Component} from "react";
-import {connect} from "react-redux";
-import {Link} from "react-router-dom";
-import {Helmet} from "react-helmet";
-import slugify from "slugify";
-import gandalfGif from "../assets/gandalf.gif";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
+import slugify from 'slugify';
 
 type Props = {
-  boards: Array < {
-    title: string,
-    id: string
-  } >
-}
+	boards: Array<{
+		title: string,
+		id: string
+	}>
+};
 
 class Home extends Component<Props> {
-  render = () => {
-    const {boards} = this.props;
-    return (<div>
-      <Helmet>
-        <title>Home | Trello</title>
-      </Helmet>
-      {
-        boards.map(board => (<Link key={board.id} to={`/b/${board.id}/${slugify(board.title, {lower: true})}`}>
-          {board.title}
-        </Link>))
-      }
-    </div>);
-  };
+	render = () => {
+		const { boards } = this.props;
+		return (
+			<div>
+				<Helmet>
+					<title>Home | Trello</title>
+				</Helmet>
+				{boards.map((board) => (
+					<Link key={board.id} to={`/b/${board.id}/${slugify(board.title, { lower: true })}`}>
+						{board.title}
+					</Link>
+				))}
+			</div>
+		);
+	};
 }
 
-const mapStateToProps = state => ({
-  boards: Object.values(state.boards)
+const mapStateToProps = (state) => ({
+	boards: Object.values(state.boards)
 });
 
 export default connect(mapStateToProps)(Home);
