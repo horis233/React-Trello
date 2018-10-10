89a0cec281261633f34110684a4bd22ad93c4bbd
horis233 <horis233@gmail.com>
2018-10-09 22:11:44 -0400

    enable editing cards

--- src/app/actionCreators.jsx
+++ src/app/actionCreators.jsx
@@ -1,25 +1,34 @@
-import axios from "axios";
-import shortid from "shortid";
+import axios from 'axios';
+import shortid from 'shortid';
 
-export const addList = (listTitle, boardId) => dispatch => {
-  const listId = shortid.generate();
-  dispatch({
-    type: "ADD_LIST",
-    payload: { listTitle, listId, boardId }
-  });
-  
-  axios
-    .post("/api/list", { listTitle, listId, boardId })
-    .then(({ data }) => console.log(data));
+export const addList = (listTitle, boardId) => (dispatch) => {
+	const listId = shortid.generate();
+	dispatch({
+		type: 'ADD_LIST',
+		payload: { listTitle, listId, boardId }
+	});
+
+	axios.post('/api/list', { listTitle, listId, boardId }).then(({ data }) => console.log(data));
 };
 
-export const addCard = (cardTitle, listId, boardId) => dispatch => {
-    const cardId = shortid.generate();
-    dispatch({
-      type: "ADD_CARD",
-      payload: { cardTitle, cardId, listId }
-    });
-     axios
-      .post("/api/card", { cardTitle, cardId, listId, boardId })
-      .then(({ data }) => console.log(data));
-  };
+export const addCard = (cardTitle, listId, boardId) => (dispatch) => {
+	const cardId = shortid.generate();
+	dispatch({
+		type: 'ADD_CARD',
+		payload: { cardTitle, cardId, listId }
+	});
+	axios.post('/api/card', { cardTitle, cardId, listId, boardId }).then(({ data }) => console.log(data));
+};
+
+export const editCardTitle = (cardTitle, cardId, list, boardId) => (dispatch) => {
+	dispatch({
+		type: 'EDIT_CARD_TITLE',
+		payload: {
+			cardTitle,
+			cardId,
+			listId: list._id
+		}
+	});
+	const cardIndex = list.cards.indexOf(cardId);
+	axios.put('/api/card', { cardTitle, cardIndex, listId: list._id, boardId }).then(({ data }) => console.log(data));
+};
