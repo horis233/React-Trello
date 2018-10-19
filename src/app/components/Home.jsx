99fa076ab7942b16b034277b5636e9b94cc6d207
horis233 <horis233@gmail.com>
2018-10-19 13:55:18 -0400

    create home boards ui with create new board button

--- src/app/components/Home.jsx
+++ src/app/components/Home.jsx
@@ -1,27 +1,51 @@
-// @flow
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
 import slugify from 'slugify';
+import './Home.scss';
 
 type Props = {
 	boards: Array<{ title: string, _id: string }>
 };
 
 class Home extends Component<Props> {
+	constructor() {
+		super();
+		this.state = {
+			isBoardAdderOpen: false
+		};
+	}
+	openBoardAdder = () => {
+		this.setState({ isBoardAdderOpen: true });
+	};
 	render = () => {
 		const { boards } = this.props;
+		const { isBoardAdderOpen } = this.state;
 		return (
-			<div>
+			<div className="home">
 				<Helmet>
 					<title>Home | Trello</title>
 				</Helmet>
-				{boards.map((board) => (
-					<Link key={board._id} to={`/b/${board._id}/${slugify(board.title, { lower: true })}`}>
-						{board.title}
-					</Link>
-				))}
+				<div className="boards">
+					<h1>My boards</h1>
+					{boards.map((board) => (
+						<Link
+							key={board._id}
+							className="board-link"
+							to={`/b/${board._id}/${slugify(board.title, { lower: true })}`}
+						>
+							{board.title}
+						</Link>
+					))}
+					{isBoardAdderOpen ? (
+						<div>Board adder!</div>
+					) : (
+						<button onClick={this.openBoardAdder} className="create-board-button">
+							Create a new board...
+						</button>
+					)}
+				</div>
 			</div>
 		);
 	};
--- src/server/fetchBoardData.js
+++ src/server/fetchBoardData.js
@@ -9,7 +9,7 @@ const normalizeBoards = (boards) => {
 };
 
 const fetchBoardData = (db) => (req, res, next) => {
-	if (req.user) {
+	if (true) {
 		const collection = db.collection('boards');
 		collection.find({}).toArray().then((boards) => {
 			req.initialState = { ...normalizeBoards(boards), user: req.user };
