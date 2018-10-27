c10d27fff3fbc5968c818d40ad9400f8a5ff3a6f
horis233 <horis233@gmail.com>
2018-10-27 16:56:44 -0400

    cleanup landing page

--- src/app/components/LandingPage.jsx
+++ src/app/components/LandingPage.jsx
@@ -1,43 +1,34 @@
 // @flow
-import React, { Component } from "react";
-// import { Link } from "react-router-dom";
-import { Helmet } from "react-helmet";
-import FaTwitter from "react-icons/lib/fa/twitter";
-import googleLogo from "../../assets/images/google-logo.svg";
-import kanbanLogo from "../../assets/images/kankan-logo.svg";
-import "./LandingPage.scss";
+import React, { Component } from 'react';
+import { Helmet } from 'react-helmet';
+import FaTwitter from 'react-icons/lib/fa/twitter';
+import googleLogo from '../../assets/images/google-logo.svg';
+import kanbanLogo from '../../assets/images/kankan-logo.svg';
+import './LandingPage.scss';
 
 class LandingPage extends Component<{}> {
-  render = () => {
-    return (
-      <div className="landing-page">
-        <Helmet>
-          <title>Landing page | Trello</title>
-        </Helmet>
-        <div className="landing-page-text">
-          <div className="landing-page-heading">
-            <img
-              src={kanbanLogo}
-              alt="kanban live logo"
-              style={{ height: 50 }}
-            />
-            &nbsp;
-            <h1>kanban.live</h1>
-          </div>
-          <h2>
-            Tagline about what it is and how it&#39;s open source and great etc
-            etc.
-          </h2>
-          <a href="/auth/twitter" className="signin-button twitter-button">
-            <FaTwitter className="logo-icon" /> &nbsp;Sign in with Twitter
-          </a>
-          <a href="/auth/google" className="signin-button google-button">
-            <img className="google-logo" src={googleLogo} alt="google logo" />
-            &nbsp;Sign in with Google
-          </a>
-        </div>
-      </div>
-    );
-  };
+	render = () => (
+		<div className="landing-page">
+			<Helmet>
+				<title>Sign in | kanban.live</title>
+			</Helmet>
+			<div className="landing-page-text">
+				<div className="landing-page-heading">
+					<img src={kanbanLogo} alt="kanban live logo" style={{ height: 50 }} />
+					&nbsp;
+					<h1>kanban.live</h1>
+				</div>
+				<h2>An open source kanban application built with React and Redux</h2>
+				<a href="/auth/twitter" className="signin-button twitter-button">
+					<FaTwitter className="logo-icon" /> &nbsp;Sign in with Twitter
+				</a>
+				<a href="/auth/google" className="signin-button google-button">
+					<img className="google-logo" src={googleLogo} alt="google logo" />
+					&nbsp;Sign in with Google
+				</a>
+			</div>
+		</div>
+	);
 }
+
 export default LandingPage;
