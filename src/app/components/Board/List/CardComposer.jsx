2212ac5c2c9de5a08d321542da0e4f7207d383ef
horis233 <horis233@gmail.com>
2018-10-28 21:12:59 -0400

    persist newCardTitle state when it gets hidden

--- src/app/components/Board/List/CardComposer.jsx
+++ src/app/components/Board/List/CardComposer.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Textarea from 'react-textarea-autosize';
 import shortid from 'shortid';
+import ClickOutside from '../../ClickOutside';
 
 type Props = {
 	boardId: string,
@@ -18,8 +19,10 @@ class CardComposer extends Component<Props> {
 		};
 	}
 
-	componentDidMount = () => {
-		setTimeout(() => this.el.scrollIntoView());
+	componentDidUpdate = (prevProps) => {
+		if (!prevProps.isOpen && this.props.isOpen) {
+			setTimeout(() => this.el.scrollIntoView());
+		}
 	};
 
 	handleChange = (event) => {
@@ -45,34 +48,40 @@ class CardComposer extends Component<Props> {
 			type: 'ADD_CARD',
 			payload: { cardTitle: newCardTitle, cardId, listId: list._id, boardId }
 		});
-		this.setState({ newCardTitle: '' });
 		toggleCardComposer();
+		this.setState({ newCardTitle: '' });
 	};
 
 	render() {
 		const { newCardTitle } = this.state;
+		const { isOpen, toggleCardComposer } = this.props;
+
 		return (
-			<form onSubmit={this.handleSubmit} className="textarea-wrapper">
-				<Textarea
-					autoFocus
-					useCacheForDOMMeasurements
-					minRows={3}
-					onChange={this.handleChange}
-					onKeyDown={this.handleKeyDown}
-					value={newCardTitle}
-					className="list-textarea"
-					spellCheck={false}
-				/>
-				<input
-					ref={(el) => {
-						this.el = el;
-					}}
-					type="submit"
-					value="Add"
-					className="submit-card-button"
-					disabled={newCardTitle === ''}
-				/>
-			</form>
+			isOpen && (
+				<ClickOutside handleClickOutside={toggleCardComposer}>
+					<form onSubmit={this.handleSubmit} className="textarea-wrapper">
+						<Textarea
+							autoFocus
+							useCacheForDOMMeasurements
+							minRows={3}
+							onChange={this.handleChange}
+							onKeyDown={this.handleKeyDown}
+							value={newCardTitle}
+							className="list-textarea"
+							spellCheck={false}
+						/>
+						<input
+							ref={(el) => {
+								this.el = el;
+							}}
+							type="submit"
+							value="Add"
+							className="submit-card-button"
+							disabled={newCardTitle === ''}
+						/>
+					</form>
+				</ClickOutside>
+			)
 		);
 	}
 }
