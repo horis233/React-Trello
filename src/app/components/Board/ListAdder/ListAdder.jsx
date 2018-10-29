ce7ca3e937cd79d7b652bc4e491fab64a8ff2a58
horis233 <horis233@gmail.com>
2018-10-29 18:11:38 -0400

    clean up ListAdder component

--- src/app/components/Board/ListAdder/ListAdder.jsx
+++ src/app/components/Board/ListAdder/ListAdder.jsx
@@ -1,72 +1,74 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import Textarea from "react-textarea-autosize";
-import shortid from "shortid";
-import "./ListAdder.scss";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Textarea from 'react-textarea-autosize';
+import shortid from 'shortid';
+import './ListAdder.scss';
 
+class ListAdder extends Component {
+	static propTypes = {
+		boardId: PropTypes.string.isRequired,
+		dispatch: PropTypes.func.isRequired
+	};
 
-class ListAdder extends Component{
-  constructor() {
-    super();
-    this.state = {
-      isListInEdit: false,
-      newListTitle: ""
-    };
-  }
-  handleBlur = () => {
-    this.setState({ isListInEdit: false });
-  };
-  handleChange = event => {
-    this.setState({ newListTitle: event.target.value });
-  };
-  handleKeyDown = event => {
-    if (event.keyCode === 13) {
-      event.preventDefault();
-      this.handleSubmit();
-    } else if (event.keyCode === 27) {
-      this.setState({ isListInEdit: false });
-    }
-  };
-  handleSubmit = () => {
-    const { dispatch, boardId } = this.props;
-    const { newListTitle } = this.state;
-    const listId = shortid.generate();
-    if (newListTitle === "") return;
-    dispatch({
-      type: "ADD_LIST",
-      payload: { listTitle: newListTitle, listId, boardId }
-    });
-    this.setState({ isListInEdit: false, newListTitle: "" });
-  };
-  render = () => {
-    const { isListInEdit, newListTitle } = this.state;
-    if (!isListInEdit) {
-      return (
-        <button
-          onClick={() => this.setState({ isListInEdit: true })}
-          className="add-list-button"
-        >
-          Add a list...
-        </button>
-      );
-    }
-    return (
-      <div className="list">
-        <div className="list-title-textarea-wrapper">
-          <Textarea
-            autoFocus
-            useCacheForDOMMeasurements
-            value={newListTitle}
-            onChange={this.handleChange}
-            onKeyDown={this.handleKeyDown}
-            className="list-title-textarea"
-            onBlur={this.handleBlur}
-            spellCheck={false}
-          />
-        </div>
-      </div>
-    );
-  };
+	constructor() {
+		super();
+		this.state = {
+			isOpen: false,
+			listTitle: ''
+		};
+	}
+	handleBlur = () => {
+		this.setState({ isOpen: false });
+	};
+	handleChange = (event) => {
+		this.setState({ listTitle: event.target.value });
+	};
+	handleKeyDown = (event) => {
+		if (event.keyCode === 13) {
+			event.preventDefault();
+			this.handleSubmit();
+		} else if (event.keyCode === 27) {
+			this.setState({ isOpen: false, listTitle: '' });
+		}
+	};
+	handleSubmit = () => {
+		const { dispatch, boardId } = this.props;
+		const { listTitle } = this.state;
+		const listId = shortid.generate();
+		if (listTitle === '') return;
+		dispatch({
+			type: 'ADD_LIST',
+			payload: { listTitle, listId, boardId }
+		});
+		this.setState({ isOpen: false, listTitle: '' });
+	};
+	render = () => {
+		const { isOpen, listTitle } = this.state;
+		if (!isOpen) {
+			return (
+				<button onClick={() => this.setState({ isOpen: true })} className="add-list-button">
+					Add a list...
+				</button>
+			);
+		}
+		return (
+			<div className="list">
+				<div className="list-title-textarea-wrapper">
+					<Textarea
+						autoFocus
+						useCacheForDOMMeasurements
+						value={listTitle}
+						onChange={this.handleChange}
+						onKeyDown={this.handleKeyDown}
+						className="list-title-textarea"
+						onBlur={this.handleBlur}
+						spellCheck={false}
+					/>
+				</div>
+			</div>
+		);
+	};
 }
 
 export default connect()(ListAdder);
