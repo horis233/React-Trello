4d839024ae938c9448b68d814527e8d8c3b6d610
horis233 <horis233@gmail.com>
2018-11-08 16:26:21 -0500

    begin standardizing styles for card and cardeditor

--- src/app/components/Board/List/Card/Calendar.jsx
+++ src/app/components/Board/List/Card/Calendar.jsx
@@ -1,62 +1,62 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
-import DayPicker from 'react-day-picker';
-import './ReactDayPicker.scss';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import DayPicker from "react-day-picker";
+import "./ReactDayPicker.scss";
 
 class Calendar extends Component {
-	static propTypes = {
-		dispatch: PropTypes.func.isRequired,
-		cardId: PropTypes.string.isRequired,
-		boardId: PropTypes.string.isRequired,
-		toggleCalendar: PropTypes.func.isRequired
-	};
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    cardId: PropTypes.string.isRequired,
+    boardId: PropTypes.string.isRequired,
+    toggleCalendar: PropTypes.func.isRequired
+  };
 
-	constructor() {
-		super();
-		this.state = {
-			selectedDay: null
-		};
-	}
+  constructor() {
+    super();
+    this.state = {
+      selectedDay: null
+    };
+  }
 
-	handleDayClick = (selectedDay, { selected }) => {
-		if (selected) {
-			// Unselect the day if already selected
-			this.setState({ selectedDay: undefined });
-			return;
-		}
-		this.setState({ selectedDay });
-	};
+  handleDayClick = (selectedDay, { selected }) => {
+    if (selected) {
+      // Unselect the day if already selected
+      this.setState({ selectedDay: undefined });
+      return;
+    }
+    this.setState({ selectedDay });
+  };
 
-	handleSave = () => {
-		const { selectedDay } = this.state;
-		const { dispatch, cardId, boardId, toggleCalendar } = this.props;
-		if (selectedDay) {
-			dispatch({
-				type: 'EDIT_CARD_DATE',
-				payload: { date: selectedDay, cardId, boardId }
-			});
-			toggleCalendar();
-		}
-	};
+  handleSave = () => {
+    const { selectedDay } = this.state;
+    const { dispatch, cardId, boardId, toggleCalendar } = this.props;
+    if (selectedDay) {
+      dispatch({
+        type: "EDIT_CARD_DATE",
+        payload: { date: selectedDay, cardId, boardId }
+      });
+      toggleCalendar();
+    }
+  };
 
-	render() {
-		const { selectedDay } = this.state;
-		const { toggleCalendar } = this.props;
-		return (
-			<div className="calendar">
-				<DayPicker
-					onDayClick={this.handleDayClick}
-					selectedDays={selectedDay}
-					disabledDays={{ before: new Date() }}
-				/>
-				<div className="calendar-buttons">
-					<button onClick={this.handleSave}>Save</button>
-					<button onClick={toggleCalendar}>Cancel</button>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    const { selectedDay } = this.state;
+    const { toggleCalendar } = this.props;
+    return (
+      <div className="calendar">
+        <DayPicker
+          onDayClick={this.handleDayClick}
+          selectedDays={selectedDay}
+          disabledDays={{ before: new Date() }}
+        />
+        <div className="calendar-buttons">
+          <button onClick={this.handleSave}>Save</button>
+          <button onClick={toggleCalendar}>Cancel</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default connect()(Calendar);
