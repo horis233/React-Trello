7e5b466b322730065b2ded2370f4fb18caf0de59
horis233 <horis233@gmail.com>
2018-11-10 23:28:37 -0500

    change 'open-card-composer-button' to an + button

--- src/app/components/Board/List/List.jsx
+++ src/app/components/Board/List/List.jsx
@@ -1,78 +1,75 @@
-import * as React from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { Draggable } from "react-beautiful-dnd";
-import ListTitle from "./ListTitle";
-import Cards from "./Cards";
-import "./List.scss";
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Draggable } from 'react-beautiful-dnd';
+import ListTitle from './ListTitle';
+import Cards from './Cards';
+import './List.scss';
 
 class List extends React.Component {
-  static propTypes = {
-    boardId: PropTypes.string.isRequired,
-    index: PropTypes.number.isRequired,
-    list: PropTypes.shape({ _id: PropTypes.string.isRequired }).isRequired
-    // cards: PropTypes.arrayOf(PropTypes.object)
-  };
+	static propTypes = {
+		boardId: PropTypes.string.isRequired,
+		index: PropTypes.number.isRequired,
+		list: PropTypes.shape({ _id: PropTypes.string.isRequired }).isRequired
+		// cards: PropTypes.arrayOf(PropTypes.object)
+	};
 
-  constructor() {
-    super();
-    this.state = {
-      cardComposerIsOpen: false
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			cardComposerIsOpen: false
+		};
+	}
 
-  toggleCardComposer = () =>
-    this.setState({ cardComposerIsOpen: !this.state.cardComposerIsOpen });
+	toggleCardComposer = () => this.setState({ cardComposerIsOpen: !this.state.cardComposerIsOpen });
 
-  render = () => {
-    const { list, boardId, index } = this.props;
-    console.log("LIST", this.props);
-    const { cardComposerIsOpen } = this.state;
-    return (
-      <Draggable
-        draggableId={list._id}
-        index={index}
-        disableInteractiveElementBlocking
-      >
-        {provided => (
-          <div>
-            <div
-              ref={provided.innerRef}
-              {...provided.draggableProps}
-              className="list-wrapper"
-            >
-              <div className="list">
-                <ListTitle
-                  dragHandleProps={provided.dragHandleProps}
-                  listTitle={list.title}
-                  listId={list._id}
-                  cards={list.cards}
-                  boardId={boardId}
-                />
-                <div className="cards-wrapper">
-                  <Cards
-                    listId={list._id}
-                    cardComposerIsOpen={cardComposerIsOpen}
-                    toggleCardComposer={this.toggleCardComposer}
-                    boardId={boardId}
-                  />
-                </div>
-                {cardComposerIsOpen || (
-                  <button
-                    onClick={this.toggleCardComposer}
-                    className="open-composer-button"
-                  >
-                    Add a card...
-                  </button>
-                )}
-              </div>
-            </div>
-            {provided.placeholder}
-          </div>
-        )}
-      </Draggable>
-    );
-  };
+	render = () => {
+		const { list, boardId, index } = this.props;
+		console.log('LIST', this.props);
+		const { cardComposerIsOpen } = this.state;
+		return (
+			<Draggable draggableId={list._id} index={index} disableInteractiveElementBlocking>
+				{(provided) => (
+					<div>
+						<div
+							ref={provided.innerRef}
+							{...provided.draggableProps}
+							className="list-wrapper"
+						>
+							<div className="list">
+								<ListTitle
+									dragHandleProps={provided.dragHandleProps}
+									listTitle={list.title}
+									listId={list._id}
+									cards={list.cards}
+									boardId={boardId}
+								/>
+								<div className="cards-wrapper">
+									<Cards
+										listId={list._id}
+										cardComposerIsOpen={cardComposerIsOpen}
+										toggleCardComposer={
+											this.toggleCardComposer
+										}
+										boardId={boardId}
+									/>
+								</div>
+								{cardComposerIsOpen || (
+									<button
+										onClick={this.toggleCardComposer}
+										className="open-composer-button"
+									>
+										+
+									</button>
+								)}
+							</div>
+						</div>
+						{provided.placeholder}
+					</div>
+				)}
+			</Draggable>
+		);
+	};
 }
 
 // const mapStateToProps = (state, ownProps) => ({
