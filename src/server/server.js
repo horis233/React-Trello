81d8d24a23e0b8bcfae4b2c852cf7d3f99371b38
horis233 <horis233@gmail.com>
2018-11-14 00:20:12 -0500

    recover

--- src/server/server.js
+++ src/server/server.js
@@ -29,7 +29,7 @@ MongoClient.connect(process.env.MONGODB_URL, { useNewUrlParser: true }).then((cl
 	app.use(favicon(path.join('dist/public/favicons/favicon.ico')));
 	app.use(express.json());
 	app.use(express.urlencoded({ extended: true }));
-	app.use('trello/static', express.static('dist/public/'));
+	app.use('/static', express.static('dist/public/'));
 
 	app.use(
 		session({
