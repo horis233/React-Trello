369512555eeeb5301cbfdd8058b01747caea43ef
horis233 <horis233@gmail.com>
2018-11-13 23:42:21 -0500

    update static file address

--- src/server/server.js
+++ src/server/server.js
@@ -29,8 +29,7 @@ MongoClient.connect(process.env.MONGODB_URL, { useNewUrlParser: true }).then((cl
 	app.use(favicon(path.join('dist/public/favicons/favicon.ico')));
 	app.use(express.json());
 	app.use(express.urlencoded({ extended: true }));
-	app.use('trello/static', express.static('dist/public/'));
-
+	app.use(express.static(__dirname + 'dist/public'));
 	app.use(
 		session({
 			store: new MongoStore({ db }),
