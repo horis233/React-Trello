547f83a087ca3a79e954c9c2b0d4a2f1ca5a50a2
horis233 <horis233@gmail.com>
2018-11-14 10:17:24 -0500

    make due-date badge cooler

--- src/app/components/Board/List/Card/CardDetails.jsx
+++ src/app/components/Board/List/Card/CardDetails.jsx
@@ -1,77 +1,77 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import format from "date-fns/format";
-import differenceInCalendarDays from "date-fns/difference_in_calendar_days";
-import MdAlarm from "react-icons/lib/md/access-alarm";
-import MdDoneAll from "react-icons/lib/fa/check-square-o";
-
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import format from 'date-fns/format';
+import differenceInCalendarDays from 'date-fns/difference_in_calendar_days';
+import MdAlarm from 'react-icons/lib/md/access-alarm';
+import MdDoneAll from 'react-icons/lib/fa/check-square-o';
 
 class CardDetails extends Component {
-  static propTypes = {
-    date: PropTypes.oneOfType([PropTypes.string, PropTypes.instanceOf(Date)]),
-    checkBoxes: PropTypes.shape({
-      total: PropTypes.number.isRequired,
-      checked: PropTypes.number.isRequired
-    }).isRequired
-  };
+	static propTypes = {
+		date: PropTypes.oneOfType([ PropTypes.string, PropTypes.instanceOf(Date) ]),
+		checkBoxes: PropTypes.shape({
+			total: PropTypes.number.isRequired,
+			checked: PropTypes.number.isRequired
+		}).isRequired
+	};
 
-  renderDueDate = () => {
-    const { date } = this.props;
-    if (!date) {
-      return null;
-    }
-    const dueDateFromToday = differenceInCalendarDays(date, new Date());
+	renderDueDate = () => {
+		const { date } = this.props;
+		if (!date) {
+			return null;
+		}
+		const dueDateFromToday = differenceInCalendarDays(date, new Date());
 
-    let dueDateString;
-    if (dueDateFromToday < -1) {
-      dueDateString = `${Math.abs(dueDateFromToday)} days ago`;
-    } else if (dueDateFromToday === -1) {
-      dueDateString = "Yesterday";
-    } else if (dueDateFromToday === 0) {
-      dueDateString = "Today";
-    } else if (dueDateFromToday === 1) {
-      dueDateString = "Tomorrow";
-    } else {
-      dueDateString = format(date, "D MMM");
-    }
+		let dueDateString;
+		if (dueDateFromToday < -1) {
+			dueDateString = `${Math.abs(dueDateFromToday)} days ago`;
+		} else if (dueDateFromToday === -1) {
+			dueDateString = 'Yesterday';
+		} else if (dueDateFromToday === 0) {
+			dueDateString = 'Today';
+		} else if (dueDateFromToday === 1) {
+			dueDateString = 'Tomorrow';
+		} else {
+			dueDateString = format(date, 'D MMM');
+		}
 
-    let dueDateColor;
-    if (dueDateFromToday < 0) {
-      dueDateColor = "red";
-    } else if (dueDateFromToday === 0) {
-      dueDateColor = "darkorange";
-    } else {
-      dueDateColor = "green";
-    }
+		let dueDateColor;
+		if (dueDateFromToday < 0) {
+			dueDateColor = 'red';
+		} else if (dueDateFromToday === 0) {
+			dueDateColor = 'darkorange';
+		} else {
+			dueDateColor = 'green';
+		}
 
-    return (
-      <div className="due-date" style={{ background: dueDateColor }}>
-        <MdAlarm className="due-date-icon" />&nbsp;
-        {dueDateString}
-      </div>
-    );
-  };
+		return (
+			<div className="due-date" style={{ background: dueDateColor }}>
+				<MdAlarm className="due-date-icon" />&nbsp;
+				{dueDateString}
+			</div>
+		);
+	};
 
-  renderTaskProgress = () => {
-    const { total, checked } = this.props.checkBoxes;
-    if (total === 0) {
-      return null;
-    }
-    return (
-      <div>
-        {checked} / {total}
-      </div>
-    );
-  };
+	renderTaskProgress = () => {
+		const { total, checked } = this.props.checkBoxes;
+		if (total === 0) {
+			return null;
+		}
+		return (
+			<div>
+				<MdDoneAll />
+				{checked} / {total}
+			</div>
+		);
+	};
 
-  render() {
-    return (
-      <div className="card-details">
-        {this.renderDueDate()}
-        {this.renderTaskProgress()}
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="card-details">
+				{this.renderDueDate()}
+				{this.renderTaskProgress()}
+			</div>
+		);
+	}
 }
 
 export default CardDetails;
