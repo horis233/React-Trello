cc08034c873fc1f0835941faca5ca9a09f7aafa8
horis233 <horis233@gmail.com>
2018-11-23 18:49:28 -0500

    remove console.logs after write operations

--- src/persistMiddleware.js
+++ src/persistMiddleware.js
@@ -6,7 +6,9 @@ const persistMiddleware = (store) => (next) => (action) => {
 	const { user, boardsById, listsById, cardsById, currentBoardId: boardId } = store.getState();
 	if (user) {
 		if (action.type === 'DELETE_BOARD') {
-			axios.delete('/api/board', { data: { boardId } }).then(({ data }) => console.log(data));
+			// All action-types that are not DELETE_BOARD or PUT_BOARD_ID_IN_REDUX are currently modifying a board in a way that should
+			// be persisted to db. If other types of actions are added, this logic has be get more specific.	      // be persisted to db. If other types of actions are added, this logic will get unwieldy.
+			axios.delete('/api/board', { data: { boardId } });
 		} else if (action.type !== 'PUT_BOARD_ID_IN_REDUX') {
 			const card = new schema.Entity('cardsById', {}, { idAttribute: '_id' });
 			const list = new schema.Entity('listsById', { cards: [ card ] }, { idAttribute: '_id' });
@@ -14,7 +16,7 @@ const persistMiddleware = (store) => (next) => (action) => {
 			const entities = { cardsById, listsById, boardsById };
 
 			const boardData = denormalize(boardId, board, entities);
-			axios.put('/api/board', boardData).then(({ data }) => console.log(data));
+			axios.put('/api/board', boardData);
 		}
 	}
 };
