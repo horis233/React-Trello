28ad9172d9e7297db3abef910e85d50a374d89e3
horis233 <horis233@gmail.com>
2018-11-29 12:35:07 -0500

    clean up Card components and rename CardEditor to CardModal

--- src/app/components/Card/CardOptions.jsx
+++ src/app/components/Card/CardOptions.jsx
@@ -1,20 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Modal from 'react-modal';
 import FaTrash from 'react-icons/lib/fa/trash';
 import MdAlarm from 'react-icons/lib/md/access-alarm';
+import Calendar from './Calendar';
 import ClickOutside from '../ClickOutside';
 import colorIcon from '../../../assets/images/color-icon.png';
+
 class CardOptions extends Component {
 	static propTypes = {
 		isColorPickerOpen: PropTypes.bool.isRequired,
 		card: PropTypes.shape({ _id: PropTypes.string.isRequired }).isRequired,
 		listId: PropTypes.string.isRequired,
 		isCardNearRightBorder: PropTypes.bool.isRequired,
+		isThinDisplay: PropTypes.bool.isRequired,
+		boundingRect: PropTypes.object.isRequired,
 		toggleColorPicker: PropTypes.func.isRequired,
-		toggleCalendar: PropTypes.func.isRequired,
 		dispatch: PropTypes.func.isRequired
 	};
+
+	constructor() {
+		super();
+		this.state = { isCalendarOpen: false };
+	}
+
 	deleteCard = () => {
 		const { dispatch, listId, card } = this.props;
 		dispatch({
@@ -22,6 +32,7 @@ class CardOptions extends Component {
 			payload: { cardId: card._id, listId }
 		});
 	};
+
 	changeColor = (color) => {
 		const { dispatch, card, toggleColorPicker } = this.props;
 		if (card.color !== color) {
@@ -31,20 +42,51 @@ class CardOptions extends Component {
 			});
 		}
 		toggleColorPicker();
+		this.colorPickerButton.focus();
 	};
+
 	handleKeyDown = (event) => {
 		if (event.keyCode === 27) {
 			this.props.toggleColorPicker();
 			this.colorPickerButton.focus();
 		}
 	};
+
 	handleClickOutside = () => {
 		const { toggleColorPicker } = this.props;
 		toggleColorPicker();
 		this.colorPickerButton.focus();
 	};
+
+	toggleCalendar = () => {
+		this.setState({ isCalendarOpen: !this.state.isCalendarOpen });
+	};
+
 	render() {
-		const { isCardNearRightBorder, isColorPickerOpen, toggleColorPicker, toggleCalendar } = this.props;
+		const {
+			isCardNearRightBorder,
+			isColorPickerOpen,
+			toggleColorPicker,
+			card,
+			isThinDisplay,
+			boundingRect
+		} = this.props;
+		const { isCalendarOpen } = this.state;
+
+		const calendarStyle = {
+			content: {
+				top: Math.min(boundingRect.bottom + 10, window.innerHeight - 300),
+				left: boundingRect.left
+			}
+		};
+
+		const calendarMobileStyle = {
+			content: {
+				top: 110,
+				left: '50%',
+				transform: 'translateX(-50%)'
+			}
+		};
 		return (
 			<div
 				className="options-list"
@@ -104,14 +146,28 @@ class CardOptions extends Component {
 					)}
 				</div>
 				<div>
-					<button onClick={toggleCalendar} className="options-list-button">
+					<button onClick={this.toggleCalendar} className="options-list-button">
 						<div className="modal-icon">
 							<MdAlarm />
 						</div>&nbsp;Due date
 					</button>
 				</div>
+				<Modal
+					isOpen={isCalendarOpen}
+					onRequestClose={this.toggleCalendar}
+					overlayClassName="calendar-underlay"
+					className="calendar-modal"
+					style={isThinDisplay ? calendarMobileStyle : calendarStyle}
+				>
+					<Calendar
+						cardId={card._id}
+						date={card.date}
+						toggleCalendar={this.toggleCalendar}
+					/>
+				</Modal>
 			</div>
 		);
 	}
 }
+
 export default connect()(CardOptions);
