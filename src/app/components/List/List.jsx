eb2c60365a8d2cc4453517675e6c31803135df5a
horis233 <horis233@gmail.com>
2018-12-03 12:33:53 -0500

    split up components into even smaller components

--- src/app/components/List/List.jsx
+++ src/app/components/List/List.jsx
@@ -1,83 +1,55 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { Draggable } from "react-beautiful-dnd";
-import classnames from "classnames";
-import ListHeader from "./ListHeader";
-import Cards from "./Cards";
-import CardComposer from "./CardComposer/CardComposer";
-import "./List.scss";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Draggable } from 'react-beautiful-dnd';
+import classnames from 'classnames';
+import ListHeader from './ListHeader';
+import Cards from './Cards';
+import CardAdder from '../CardAdder/CardAdder';
+import './List.scss';
 
 class List extends Component {
-  static propTypes = {
-    boardId: PropTypes.string.isRequired,
-    index: PropTypes.number.isRequired,
-    list: PropTypes.shape({ _id: PropTypes.string.isRequired }).isRequired
-  };
+	static propTypes = {
+		boardId: PropTypes.string.isRequired,
+		index: PropTypes.number.isRequired,
+		list: PropTypes.shape({ _id: PropTypes.string.isRequired }).isRequired
+	};
 
-  constructor() {
-    super();
-    this.state = {
-      cardComposerIsOpen: false
-    };
-  }
-
-  toggleCardComposer = () =>
-    this.setState({ cardComposerIsOpen: !this.state.cardComposerIsOpen });
-
-  render = () => {
-    const { list, boardId, index } = this.props;
-    const { cardComposerIsOpen } = this.state;
-    return (
-      <Draggable
-        draggableId={list._id}
-        index={index}
-        disableInteractiveElementBlocking
-      >
-        {(provided, snapshot) => (
-          <>
-            <div
-              ref={provided.innerRef}
-              {...provided.draggableProps}
-              className="list-wrapper"
-            >
-              <div
-                className={classnames("list", {
-                  "list--drag": snapshot.isDragging
-                })}
-              >
-                <ListHeader
-                  dragHandleProps={provided.dragHandleProps}
-                  listTitle={list.title}
-                  listId={list._id}
-                  cards={list.cards}
-                  boardId={boardId}
-                />
-                <div className="cards-wrapper">
-                  <Cards listId={list._id} />
-                </div>
-              </div>
-              {cardComposerIsOpen ? (
-                <CardComposer
-                  isOpen={cardComposerIsOpen}
-                  toggleCardComposer={this.toggleCardComposer}
-                  listId={list._id}
-                />
-              ) : (
-                <button
-                  onClick={this.toggleCardComposer}
-                  className="open-composer-button"
-                >
-                  +
-                </button>
-              )}
-            </div>
-            {provided.placeholder}
-          </>
-        )}
-      </Draggable>
-    );
-  };
+	render = () => {
+		const { list, boardId, index } = this.props;
+		return (
+			<Draggable draggableId={list._id} index={index} disableInteractiveElementBlocking>
+				{(provided, snapshot) => (
+					<div>
+						<div
+							ref={provided.innerRef}
+							{...provided.draggableProps}
+							className="list-wrapper"
+						>
+							<div
+								className={classnames('list', {
+									'list--drag': snapshot.isDragging
+								})}
+							>
+								<ListHeader
+									dragHandleProps={provided.dragHandleProps}
+									listTitle={list.title}
+									listId={list._id}
+									cards={list.cards}
+									boardId={boardId}
+								/>
+								<div className="cards-wrapper">
+									<Cards listId={list._id} />
+								</div>
+							</div>
+							<CardAdder listId={list._id} />
+						</div>
+						{provided.placeholder}
+					</div>
+				)}
+			</Draggable>
+		);
+	};
 }
 
 export default connect()(List);
